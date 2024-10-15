37b7fe2dbad642b317694d6090e6da2514302071
Diyorbek3716 <laylomusayeva546@gmail.com>
2024-10-15 21:36:48 +0500

    AddArchtectureTwo

---
+++ DiyoShop.API/DiyoShop.Core/EntityModels/BaseEntity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiyoShop.Core.EntityModels
+{
+	public class BaseEntity:IHasIsDelededModel
+	{
+		public Guid Id { get; set; }
+		[AllowNull]
+		public DateTime CreateDate { get; set; }
+		[AllowNull]
+		public DateTime UpdateDate { get; set; } 
+		[AllowNull]
+		public Guid CreateBy { get; set; }
+		[AllowNull]
+		public Guid UpdateBy { get; set; }
+		public bool IsDeleted { get; set; }
+	}
+}
---
+++ DiyoShop.API/DiyoShop.Persistence/EtityDbContextModels/EntityDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiyoShop.Persistence.EtityDbContextModels
+{
+	public class EntityDbContext:DbContext
+	{
+        public EntityDbContext(DbContextOptions<EntityDbContext> options) : base(options) { }
+
+        
+    }
+}
